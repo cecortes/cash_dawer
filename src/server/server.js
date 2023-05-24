0de1b8af511298d48ed6d0897ca314b1d7946502
César López <cesarlopezcortes@hotmail.com>
2023-05-24 10:22:39 -0600

    Server IO tested and running ok

--- src/server/server.js
+++ src/server/server.js
@@ -4,13 +4,33 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+var path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 io.on("connection", (socket) => {
+  // User connected
   console.log("a user connected");
+
+  // Listen for incoming data drawer
+  socket.on("drawer", (data) => {
+    // Debug incoming data
+    console.log(`>> Rx: ${data.drawer}`);
+
+    // If the incoming data is "open" then open the cash drawer
+    if (data.drawer === "open") {
+      console.log(">> Opening cash drawer");
+    }
+
+    // If the incoming data is "close" then close the cash drawer
+    if (data.drawer === "close") {
+      console.log(">> Closing cash drawer");
+    }
+  });
 });
 
 server.listen(3000, () => {
